fd0fc5b95fab35798eb1e0ee942542bf36041ec8
Ammar Husain <ammar_husain@apple.com>
2016-10-28 13:51:31 -0700

    Added a barebones Hello World

---
+++ main.cpp
@@ -0,0 +1,11 @@
+// Copyright (C) 2016 Apple Inc. All Rights Reserved.
+/// \author: Ammar Husain (mrahusain@gmail.com)
+
+#include <iostream>  // std::cout
+
+int main(int argc, char **argv) {
+
+  std::cout << "Hello world!" << std::endl;
+
+  return 0;
+}
